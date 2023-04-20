a7a846254b251f192f31d44caee257551829d640
Robert Chatley <rbc@imperial.ic.ac.uk>
2023-04-20 14:11:40 +0100

    Log query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,6 +4,8 @@ public class QueryProcessor {
 
   public String process(String query) {
 
+    System.out.println("Received query:" + query);
+
     if (query.toLowerCase().contains("shakespeare")) {
       return "William Shakespeare (26 April 1564 - 23 April 1616) was an "
           + "English poet, playwright, and actor, widely regarded as the greatest "
