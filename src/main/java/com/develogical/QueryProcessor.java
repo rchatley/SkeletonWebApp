f5f5671e0ad226b77a86f909e859be15d3d489e8
Robert Chatley <rbc@imperial.ac.uk>
2025-04-04 16:09:27 +0100

    Reset to starting point.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -11,9 +11,6 @@ public class QueryProcessor {
           + "English poet, playwright, and actor, widely regarded as the greatest "
           + "writer in the English language and the world's pre-eminent dramatist.";
     }
-    if (query.toLowerCase().contains("name")) {
-      return "RobTest";
-    }
 
     return "";
   }
